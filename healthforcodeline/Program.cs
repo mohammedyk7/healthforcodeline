d0698ba0f4c9e0bfcdf6ea7d68d47167477ff63f
Ishaq AL balushi GTR <ishaqalbalushi693@gmail.com>
2025-08-01 21:17:02 +0400

    add  Program

---
+++ healthforcodeline/Program.cs
@@ -0,0 +1,39 @@
+﻿using hospitalsystem.Interface;
+using hospitalsystem.models;
+using hospitalsystem.services;
+using hospitalsystem.Services;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Load data from files
+        HospitalData.Doctors = FileStorage.LoadFromFile<Doctor>("doctors.json");
+        HospitalData.Patients = FileStorage.LoadFromFile<Patient>("patients.json");
+        HospitalData.Bookings = FileStorage.LoadFromFile<Booking>("bookings.json");
+        HospitalData.Records = FileStorage.LoadFromFile<PatientRecord>("records.json");
+        HospitalData.Branches = FileStorage.LoadFromFile<Branch>("branches.json");
+        HospitalData.Departments = FileStorage.LoadFromFile<Department>("departments.json");
+        HospitalData.Clinics = FileStorage.LoadFromFile<Clinic>("clinics.json");
+
+        // Ensure at least one test patient exists
+        if (!HospitalData.Patients.Any(p => p.Email == "yusuf@patient.com"))
+        {
+            HospitalData.Patients.Add(new Patient("Yusuf", "yusuf@patient.com", "123"));
+            FileStorage.SaveToFile("patients.json", HospitalData.Patients);
+        }
+
+    
+
+
+
+        SuperAdminService.RunMainMenu();
+
+   
+
+}
+  
+
+}
+
