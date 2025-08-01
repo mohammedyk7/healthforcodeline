de5f8642e3da7f437df4d428644b734dc190b81d
Mohammed Alkhusaibi <mohammedyk7@gmail.com>
2025-08-01 20:35:52 +0400

    module for clinic

---
+++ healthforcodeline/Modules/Clinic.cs
@@ -0,0 +1,35 @@
+﻿namespace hospitalsystem.models
+{
+    public class Clinic
+    {
+
+    
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+        public int BranchId { get; set; }
+
+        public Clinic() { }  
+
+        public Clinic(int id, string name, int branchId)
+        {
+            Id = id;
+            Name = name;
+            BranchId = branchId;
+        }
+        public Clinic(int id, string name, int departmentId, int branchId)
+        {
+            Id = id;
+            Name = name;
+            DepartmentId = departmentId;
+            BranchId = branchId;
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Clinic #{Id}: {Name} (Branch ID: {BranchId})");
+        }
+    }
+}
