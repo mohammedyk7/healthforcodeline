a8def54b9176ca3f9d9f1c04426d4d9785807776
Mohammed Alkhusaibi <mohammedyk7@gmail.com>
2025-08-01 20:34:06 +0400

    adding interfaces for doctor service

---
+++ healthforcodeline/Interface/IDoctorService.cs
@@ -0,0 +1,17 @@
+﻿using hospitalsystem.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace hospitalsystem.Interface
+{
+    public interface IDoctorService
+    {
+       
+       public void DisplayDoctorMenu();
+       
+    }
+}
