6f876a23e03d0c8007fdbbb8e10d87cca5b641cb
Mohammed Alkhusaibi <mohammedyk7@gmail.com>
2025-08-01 20:38:11 +0400

    Add files via upload

---
+++ healthforcodeline/Modules/User.cs
@@ -0,0 +1,21 @@
+﻿namespace hospitalsystem.models
+{
+    public abstract class User
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public User() { }
+
+        public User(string fullName, string email, string password)
+        {
+            FullName = fullName;
+            Email = email;
+            Password = password;
+        }
+
+        public abstract void DisplayMenu();
+    }
+}
