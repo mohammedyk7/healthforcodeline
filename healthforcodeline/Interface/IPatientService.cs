d3903f1c060f3532c1303e0c130a0bae72aa6596
Ishaq AL balushi GTR <ishaqalbalushi693@gmail.com>
2025-08-01 21:07:20 +0400

    add  IPatientService

---
+++ healthforcodeline/Interface/IPatientService.cs
@@ -0,0 +1,15 @@
+﻿using hospitalsystem.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospitalsystem.Interface
+{
+    public interface IPatientService
+    {
+        void DisplayPatientMenu();  // <- MUST BE INCLUDED
+        // Add more method signatures as needed
+    }
+}
