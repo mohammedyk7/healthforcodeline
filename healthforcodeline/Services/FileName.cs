4228e49689a8501ba53976030a6c944c542d7d34
lenovo <mohammedyk7@gmail.com>
2025-08-01 20:25:09 +0400

    Add project files.

---
+++ healthforcodeline/Services/FileName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthforcodeline.Services
+{
+    internal class FileName
+    {
+    }
+}
