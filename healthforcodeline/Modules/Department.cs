f80133a534a80d2e4a3598cfafb137874b34249f
Mohammed Alkhusaibi <mohammedyk7@gmail.com>
2025-08-01 20:36:31 +0400

    module for department

---
+++ healthforcodeline/Modules/Department.cs
@@ -0,0 +1,21 @@
+﻿namespace hospitalsystem.models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Department #{Id}: {Name}");
+        }
+    }
+}
