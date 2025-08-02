f9f9fefeb16203ddc79175d93d17bab6fefee7a5
lenovo <mohammedyk7@gmail.com>
2025-08-02 12:56:42 +0400

    Saves the updated doctors list to a JSON file

--- healthforcodeline/Modules/Doctor.cs
+++ healthforcodeline/Modules/Doctor.cs
@@ -149,7 +149,7 @@ namespace hospitalsystem.models
 
             var doctor = new Doctor(name, email, "123", clinicId);// Creates a new Doctor object with the provided details
             HospitalData.Doctors.Add(doctor);// Adds the new doctor to the hospital data doctors list
-            FileStorage.SaveToFile("doctors.json", HospitalData.Doctors);
+            FileStorage.SaveToFile("doctors.json", HospitalData.Doctors);// Saves the updated doctors list to a JSON file
 
             Console.WriteLine("Doctor added successfully.\n");
         }
