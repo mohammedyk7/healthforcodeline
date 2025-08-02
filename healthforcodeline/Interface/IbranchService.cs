5b106cd971c781d43b7a4b3d35936fae8ae64752
lenovo <mohammedyk7@gmail.com>
2025-08-02 12:23:17 +0400

    Create a new branch

--- healthforcodeline/Interface/IbranchService.cs
+++ healthforcodeline/Interface/IbranchService.cs
@@ -10,7 +10,7 @@ namespace hospitalSystem.Interface
     // IBranchService.cs
     public interface IBranchService // Interface for branch management operations
     {
-        void CreateBranch(Branch branch);
+        void CreateBranch(Branch branch);// Create a new branch
         List<Branch> GetAllBranches();
         Branch? GetBranchById(int id);
         void DeleteBranch(int id);
