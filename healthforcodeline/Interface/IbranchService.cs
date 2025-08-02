2e70f6c7ee025e77db47037200baa57bffa32040
lenovo <mohammedyk7@gmail.com>
2025-08-02 12:23:35 +0400

    Retrieve all branches

--- healthforcodeline/Interface/IbranchService.cs
+++ healthforcodeline/Interface/IbranchService.cs
@@ -11,7 +11,7 @@ namespace hospitalSystem.Interface
     public interface IBranchService // Interface for branch management operations
     {
         void CreateBranch(Branch branch);// Create a new branch
-        List<Branch> GetAllBranches();
+        List<Branch> GetAllBranches();// Retrieve all branches
         Branch? GetBranchById(int id);
         void DeleteBranch(int id);
         void UpdateBranch(Branch branch);
